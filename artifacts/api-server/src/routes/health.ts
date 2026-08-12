2f1de7b85447426433e2d7985fda2b5049b4bd71
AkmalKurniawan <148427738+AkmalKurniawan007@users.noreply.github.com>
2026-08-12 10:13:41 +0700

    Update health.ts

--- artifacts/api-server/src/routes/health.ts
+++ artifacts/api-server/src/routes/health.ts
@@ -1,10 +1,10 @@
-import { Router, type IRouter, Request, Response } from "express";
+import { Router, type IRouter } from "express";
+import type { Request, Response } from "express";
 import { HealthCheckResponse } from "@workspace/api-zod";
 
 const router: IRouter = Router();
 
-// Tambahkan tipe Request dan Response di sini
-router.get("/healthz", (_req: Request, res: Response) => {
+router.get("/healthz", (req: Request, res: Response) => {
   const data = HealthCheckResponse.parse({ status: "ok" });
   res.json(data);
 });
