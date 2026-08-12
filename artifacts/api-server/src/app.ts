1811e727f53d5e9c90780aedb86f77d44f702747
AkmalKurniawan <148427738+AkmalKurniawan007@users.noreply.github.com>
2026-08-12 10:04:06 +0700

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,8 +1,9 @@
-import express, { type Express } from "express";
+import express, { type Express, Request, Response } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
+
 
 const app: Express = express();
 
@@ -10,14 +11,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
